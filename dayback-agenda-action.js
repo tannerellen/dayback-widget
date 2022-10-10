42a87d01601276f79ac1588185450f097f39462b
Tanner Ellen <tanner@seedcode.com>
2022-10-09 19:59:20 -0700

    Add check to send events only if they are done loading

--- dayback-agenda-action.js
+++ dayback-agenda-action.js
@@ -1,4 +1,4 @@
-// DayBack Agenda Widget Service v 0.1
+// DayBack Agenda Widget Service v0.2.0
 // License: MIT
 
 // Purpose:
@@ -7,39 +7,43 @@
 // Action Type: After Events Rendered
 // Open in new window: No
 
-if (typeof agendaCheck !== "undefined" && !params.data.fromViewChange) {
+if (typeof agendaCheck !== "undefined") {
   try {
-    // Only run if the agendaCheck function has been defined from Scriptable
-    agendaCheck(function () {
-      const config = seedcodeCalendar.get("config");
-      const events = seedcodeCalendar
-        .get("element")
-        .fullCalendar("clientEvents");
-      let eventResult = [];
+    let loadingEvents = seedcodeCalendar.get('loading-events');
+    if (!loadingEvents) {
+      // Only run if the events have finsished loading 
+      // Only run if the agendaCheck function has been defined from Scriptable
+      agendaCheck(function () {
+        const config = seedcodeCalendar.get("config");
+        const events = seedcodeCalendar
+          .get("element")
+          .fullCalendar("clientEvents");
+        let eventResult = [];
 
-      for (let i = 0; i < events.length; i++) {
-        if (!config.eventShown(events[i])) {
-          // If the event isn't shown don't add it to to the list
-          continue;
+        for (let i = 0; i < events.length; i++) {
+          if (!config.eventShown(events[i])) {
+            // If the event isn't shown don't add it to to the list
+            continue;
+          }
+          const timeDisplay = events[i].allDay
+            ? ""
+            : events[i].start.format("h:mm a") + " ";
+          const title = events[i].title;
+          const event = {
+            title: title.replace("\n", " | "),
+            timeDisplay: timeDisplay,
+            color: events[i].color,
+            allDay: events[i].allDay,
+            start: events[i].start.format(),
+            end: events[i].end.format(),
+            sort: events[i].start.valueOf(),
+          };
+          eventResult.push(event);
         }
-        const timeDisplay = events[i].allDay
-          ? ""
-          : events[i].start.format("h:mm a") + " ";
-        const title = events[i].title;
-        const event = {
-          title: title.replace("\n", " | "),
-          timeDisplay: timeDisplay,
-          color: events[i].color,
-          allDay: events[i].allDay,
-          start: events[i].start.format(),
-          end: events[i].end.format(),
-          sort: events[i].start.valueOf(),
-        };
-        eventResult.push(event);
-      }
-      // Send results back to scriptable
-      completion(JSON.stringify(eventResult));
-    });
+        // Send results back to scriptable
+        completion(JSON.stringify(eventResult));
+      });
+    }
   } catch (error) {
     if (typeof completion !== "undefined") {
       completion(error);
