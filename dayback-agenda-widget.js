592268d9227ff9170b1b3810e0f680c400633ae8
Tanner Ellen <tanner@seedcode.com>
2026-09-21 00:50:57 -0700

    Testing version check

--- dayback-agenda-widget.js
+++ dayback-agenda-widget.js
@@ -1,7 +1,7 @@
 /* *****************************************
 Name	: dayback-agenda-widget.js
 Author	: Tanner Ellen
-Version	: 2.2.0
+Version	: 2.2.1
 Desc	: An iOS widget to display a daily agenda from DayBack using Scriptable.app
 ***************************************** */
 
